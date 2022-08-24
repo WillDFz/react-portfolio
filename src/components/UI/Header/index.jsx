44a78a8063fa8e54f8b672fe9285925ba5922f41
WillDFz <willwow@outlook.com>
2022-08-24 01:22:40 -0300

    First commit

---
+++ src/components/UI/Header/index.jsx
@@ -0,0 +1,20 @@
+// components
+import { Col, Container, Row } from "react-bootstrap";
+// styles
+import "./styles.module.scss";
+
+const Header = () => {
+    return (
+        <header>
+            <Container>
+                <Row>
+                    <Col className="text-center">Will</Col>
+                    <Col className="text-center">Will</Col>
+                </Row>
+
+            </Container>
+        </header>
+    );
+};
+
+export default Header;
