eb2762ed4a0d1fa88c21721f9e996458b3454e3a
WillDFz <willwow@outlook.com>
2022-09-03 04:23:58 -0300

    Fix: Social links target

--- src/components/SocialLinks/index.jsx
+++ src/components/SocialLinks/index.jsx
@@ -1,17 +1,27 @@
 import React from "react";
 
-import styles from './styles.module.scss';
+import styles from "./styles.module.scss";
 
 function SocialLinks() {
     return (
-        <div className={`${styles.linksContainer} d-flex justify-content-center my-3 py-2`}>
+        <div
+            className={`${styles.linksContainer} d-flex justify-content-center my-3 py-2`}
+        >
             <div className={`${styles.linkWrapper} mx-2`}>
-                <a href="https://www.linkedin.com/in/william-dourado-143527170/">
-                    <img src="svg/linkedin.svg" target="_blank" alt="" />
+                <a
+                    href="https://www.linkedin.com/in/william-dourado-143527170/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <img src="svg/linkedin.svg" alt="" />
                 </a>
             </div>
             <div className={`${styles.linkWrapper} mx-2`}>
-                <a href="https://github.com/WillDFz">
+                <a
+                    href="https://github.com/WillDFz"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
                     <img src="svg/github.svg" target="_blank" alt="" />
                 </a>
             </div>
